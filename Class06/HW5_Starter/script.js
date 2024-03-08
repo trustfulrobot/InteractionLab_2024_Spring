a4f683c7a8e0aeccd44ddc4fdd405bc6e36f41f5
Patrick Morrissey <patrick@trustfulrobot.com>
2024-03-08 10:44:49 -0500

    hoist variables to top

--- Class06/HW5_Starter/script.js
+++ Class06/HW5_Starter/script.js
@@ -2,6 +2,17 @@ const containerEl = document.querySelector("#blocks_container");
 const createBlocksBtn = document.querySelector("#create_blocks_btn");
 const removeBlocksBtn = document.querySelector("#remove_blocks_btn");
 
+const foreground_colors = [
+  "blue",
+  "red",
+  "green"
+];
+const background_colors = [
+  "yellow",
+  "orange",
+  "brown"
+];
+
 console.log(containerEl)
 
 function createBlocks() {
@@ -17,17 +28,7 @@ function removeBlocks() {
 createBlocksBtn.addEventListener("click", createBlocks);
 removeBlocksBtn.addEventListener("click", removeBlocks);
 
-const foreground_colors = [
-  "blue",
-  "red",
-  "green"
-]
-const background_colors = [
-  "yellow",
-  "orange",
-  "brown"
-]
 
-for(let i = 0; i < foregound_colors.length; i++) {
-  console.log("color: ", foregound_colors[i]);
+for(let i = 0; i < foreground_colors.length; i++) {
+  console.log("color: ", foreground_colors[i]);
 }
