60687948ff5252c505b79b0a23832911db55dbdf
Patrick Morrissey <patrick@trustfulrobot.com>
2024-03-08 07:32:18 -0500

    Add comment

--- Class07/1-Map/script.js
+++ Class07/1-Map/script.js
@@ -1,5 +1,6 @@
 const fahrenheitTemps = [ 88,65,92,78,66,68,80,85,90,93 ];
 
+// formula taken from https://www.metric-conversions.org/temperature/fahrenheit-to-celsius.htm
 function fahrenheitToCelsius(temp){
     return (temp - 32) / 1.8;
 }
