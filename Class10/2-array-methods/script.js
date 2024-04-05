249f341854281aef32ff326888aa488cf06e75d9
Patrick Morrissey <patrick@trustfulrobot.com>
2024-04-05 11:28:23 -0400

    Fixed js

--- Class10/2-array-methods/script.js
+++ Class10/2-array-methods/script.js
@@ -1,51 +1,56 @@
-const numbersArr = [3, 10, 4, 21, 5, 9, 2, 6, 5, 3, 5];
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Apple"];
+const vegetables = ["Tomato", "Celery", "Avocado", "Onion"];
 
-// Ascending Order
-numbersArr.sort((a, b) => a - b);
-console.log(numbersArr); // Output: [2,3,3,4,5,5,5,6,9,10,21]
+// The pop() method removes the last element from an array
+// The pop() method returns the value that was "popped out"
 
-// Descending Order
-numbersArr.sort((a, b) => b - a);
-console.log(numbersArr); // Output: [21,10,9,6,5,5,5,4,3,3,2]
-
-
-// How the JavaScript Array sort() Method Works
-
-// let numArray = [3, 4, 1, 7, 2];
-// let sortedArr = numArray.sort();
-// console.log(sortedArr); // Output: [1,2,3,4,7]
-
-// let numArray = [3, 4, 1, 7, 2];
-// numArray.sort();
-// console.log(numArray); // Output: [1,2,3,4,7]
-
-// let numArray = [3, 4, 1, 7, 2].sort();
-// console.log(numArray); // Output: [1,2,3,4,7]
-
-// let numArray = [3, 10, 4, 21, 5, 9, 2, 6, 5, 3, 5].sort();
-// console.log(numArray); // Output: [10,2,21,3,3,4,5,5,5,6,9]
+// fruits.pop();
+// console.log("fruits: ", fruits);
 
 
 
-// JavaScript sort() Method Shortcomings And How To Sort Accurately
+// The push() method adds a new element to an array (at the end)
+// The push() method returns the new array length
 
-// let numArr = [10, 5, 80].sort();
-// console.log(numArr); // Output: [10, 5, 80]
-
-// let numArr = [10, 5, 80];
-// numArr.sort((a, b) => a - b);
-// console.log(numArr); // Output: [5, 10, 80]
-
-// let numArr = [10, 5, 80];
-// numArr.sort((a, b) => b - a);
-// console.log(numArr); // Output: [80, 10, 5]
+// fruits.push("Kiwi");
+// console.log("fruits: ", fruits);
 
 
 
-// How to Avoid Modifying the Original Array When Sorting
+// The shift() method removes the first array element and "shifts" all other elements to a lower index.
+// The shift() method returns the value that was "shifted out"
 
-// let originalArray = [2, 1, 3];
-// let sortedArray = originalArray.slice().sort((a, b) => a - b);
+// let fruit = fruits.shift();
+// console.log("fruit: ", fruit);
+// console.log("fruits: ", fruits);
 
-// console.log(originalArray); // Output: [2, 1, 3]
-// console.log(sortedArray); // Output: [1, 2, 3]
+
+
+// The unshift() method adds a new element to an array (at the beginning), and "unshifts" older elements
+// The unshift() method returns the new array length
+
+// let moreFruits =  fruits.unshift("Lemon");
+// console.log("fruits: ", fruits);
+// console.log("moreFruits: ", moreFruits);
+
+
+
+// The indexOf() method returns the first index (position) of a specified value.
+// The indexOf() method returns -1 if the value is not found.
+
+// let applePosition = fruits.indexOf("Apple");
+// let kiwiPosition = fruits.indexOf("Kiwi");
+// console.log("applePosition: ", applePosition);
+// console.log("kiwiPosition: ", kiwiPosition);
+
+
+let applePosition = fruits.lastIndexOf("Apple");
+console.log("applePosition: ", applePosition);
+
+
+
+// The concat() method creates a new array by merging (concatenating) existing arrays
+
+// let fruitsAndVegetables = fruits.concat(vegetables);
+// let fruitsAndVegetables = vegetables.concat(fruits);
+// console.log("fruitsAndVegetables: ", fruitsAndVegetables);
