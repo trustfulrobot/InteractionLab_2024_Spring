221a68e3579baa0d729c6f742888ac13f3161270
Patrick Morrissey <patrick@trustfulrobot.com>
2024-05-03 07:36:39 -0400

    Class 14

---
+++ Class14/1-Animations-with-gsap/script.js
@@ -0,0 +1,26 @@
+gsap.to(".box", { 
+    x: 200,
+});
+
+// gsap.to(".circle", { 
+//     x: 40, 
+//     fill: 'blue', 
+// });
+
+// gsap.from(".circle", { 
+//     x: -40, 
+//     fill: 'blue', 
+// });
+
+// gsap.fromTo( ".circle",{ 
+//     x: -40, 
+//     fill: 'blue', 
+// }, { 
+//     x: 40, 
+//     fill: 'green' 
+// });
+
+// gsap.set(".circle", { 
+//     xPercent: 40, 
+//     fill: 'blue', 
+// });
